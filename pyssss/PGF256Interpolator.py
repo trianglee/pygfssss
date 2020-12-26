6a1f9b798e10a2670aca4ce639d238c8a0f86131
Nimrod Zimerman <zimerman@fastmail.fm>
2020-12-26 22:07:35 +0200

    Reformat files to match PEP8

--- pyssss/PGF256Interpolator.py
+++ pyssss/PGF256Interpolator.py
@@ -18,59 +18,60 @@
 from pyssss.GF256elt import GF256elt
 from pyssss.PGF256 import PGF256
 
+
 class PGF256Interpolator:
-  """Lagrange Polynomial Interpolator.
+    """Lagrange Polynomial Interpolator.
      see http://en.wikipedia.org/wiki/Lagrange_polynomial"""
-  
-  def interpolate(self,points):
-    """Returns a PGF256 polynomial interpolating all GF256xGF256 tuples in points."""
 
-    #
-    # Check that all points have different X
-    #
-    
-    for i in range(0,len(points)):
-      if points[i][0] in map(lambda x: x[0],points[i+1:]):
-        raise Exception("Duplicate point exception")
-        
-    #
-    # Special case for k=2
-    #
+    def interpolate(self, points):
+        """Returns a PGF256 polynomial interpolating all GF256xGF256 tuples in points."""
 
-    if (2 == len(points)):
-      x = PGF256((GF256elt(0),GF256elt(1)))
-      P = PGF256([points[1][1]]) * (x - points[0][0]) * (GF256elt(1) / (points[1][0] - points[0][0]))
-      P = P + PGF256([points[0][1]]) * (x - points[1][0]) * (GF256elt(1) / (points[0][0] - points[1][0]));
-      return P
+        #
+        # Check that all points have different X
+        #
 
-    #
-    # Build the interpolating polynomial
-    #
-    # L(x) = sigma(j=0,j <= k,yj * Lj(x))
-    # Where k = len(points) - 1
-    # and Lj(x) = pi(i=0,i <= k and i != j, (x-xi)/(xj - xi))
-    # Lj(xi) = kronecker_delta(i,j)
-    # 
-    
-    result = PGF256([GF256elt(0)])
-    
-    for j in range(0,len(points)):
-      result = result + (self.__Lj(points,j) * points[j][1])
+        for i in range(0, len(points)):
+            if points[i][0] in map(lambda x: x[0], points[i + 1:]):
+                raise Exception("Duplicate point exception")
 
-    return result
-              
-  def __Lj(self,points,j):
-    result = GF256elt(1)
-    x = PGF256((GF256elt(0),GF256elt(1)))
-    for i in range(0,len(points)):
-      if j == i:
-        continue
-      
-      # P = x
-      P = x
-      
-      P = (P - points[i][0]) * (GF256elt(1) / (points[j][0] - points[i][0])) 
+        #
+        # Special case for k=2
+        #
 
-      result = P * result
-                    
-    return result
+        if (2 == len(points)):
+            x = PGF256((GF256elt(0), GF256elt(1)))
+            P = PGF256([points[1][1]]) * (x - points[0][0]) * (GF256elt(1) / (points[1][0] - points[0][0]))
+            P = P + PGF256([points[0][1]]) * (x - points[1][0]) * (GF256elt(1) / (points[0][0] - points[1][0]))
+            return P
+
+        #
+        # Build the interpolating polynomial
+        #
+        # L(x) = sigma(j=0,j <= k,yj * Lj(x))
+        # Where k = len(points) - 1
+        # and Lj(x) = pi(i=0,i <= k and i != j, (x-xi)/(xj - xi))
+        # Lj(xi) = kronecker_delta(i,j)
+        #
+
+        result = PGF256([GF256elt(0)])
+
+        for j in range(0, len(points)):
+            result = result + (self.__Lj(points, j) * points[j][1])
+
+        return result
+
+    def __Lj(self, points, j):
+        result = GF256elt(1)
+        x = PGF256((GF256elt(0), GF256elt(1)))
+        for i in range(0, len(points)):
+            if j == i:
+                continue
+
+            # P = x
+            P = x
+
+            P = (P - points[i][0]) * (GF256elt(1) / (points[j][0] - points[i][0]))
+
+            result = P * result
+
+        return result
--- pyssss/PySSSS.py
+++ pyssss/PySSSS.py
@@ -23,140 +23,139 @@ from pyssss.PGF256 import PGF256
 from pyssss.PGF256Interpolator import PGF256Interpolator
 
 
-def pickRandomPolynomial(degree, value):
-  """Pick a random PGF256 polynomial P such that P(0) = value"""
+def pick_random_polynomial(degree, value):
+    """Pick a random PGF256 polynomial P such that P(0) = value"""
 
-  coeffs = []
+    coeffs = []
 
-  # Set f(0)
-  coeffs.append(value)
+    # Set f(0)
+    coeffs.append(value)
 
-  # Pick coefficients for x^n with n <= degree
+    # Pick coefficients for x^n with n <= degree
 
-  for c in range(1,degree+1):
-    coeffs.append(GF256elt(random.randint(0,255)))
+    for c in range(1, degree + 1):
+        coeffs.append(GF256elt(random.randint(0, 255)))
 
-  return PGF256(coeffs)
+    return PGF256(coeffs)
 
 
 def pick_random_x_values(n):
-  # Allocate array to track duplicates
-  picked = [False for i in range(0,256)]
+    # Allocate array to track duplicates
+    picked = [False for i in range(0, 256)]
 
-  picks = []
+    picks = []
 
-  for i in range(0,n):
+    for i in range(0, n):
 
-    # Pick a not yet picked X value in [1,255],
-    while True:
-      pick = random.randint(1, 255)
-      if not picked[pick]:
-        break
+        # Pick a not yet picked X value in [1,255],
+        while True:
+            pick = random.randint(1, 255)
+            if not picked[pick]:
+                break
 
-    # Keep track of the value we just picked
-    picked[pick] = True
+        # Keep track of the value we just picked
+        picked[pick] = True
 
-    picks.append(pick)
+        picks.append(pick)
 
-  return picks
+    return picks
 
 
-def encodeByte(byte, n, k, x_values):
-  # Pick a random polynomial
-  P = pickRandomPolynomial(k-1, GF256elt(byte))
+def encode_byte(byte, n, k, x_values):
+    # Pick a random polynomial
+    P = pick_random_polynomial(k - 1, GF256elt(byte))
 
-  # Generate the keys
-  keys = [bytearray() for i in range(0,n)]
+    # Generate the keys
+    keys = [bytearray() for i in range(0, n)]
 
-  for i in range(0,n):
-    X = GF256elt(x_values[i])
-    Y = P.f(X)
+    for i in range(0, n):
+        X = GF256elt(x_values[i])
+        Y = P.f(X)
 
-    keys[i] += bytes([int(Y)])
+        keys[i] += bytes([int(Y)])
 
-  return keys
+    return keys
 
 
 def encode(stream, outputs, k):
+    n = len(outputs)
 
-  n = len(outputs)
+    x_values = pick_random_x_values(n)
+    for i in range(0, n):
+        outputs[i].write(bytes([x_values[i]]))
 
-  x_values = pick_random_x_values(n)
-  for i in range(0,n):
-    outputs[i].write(bytes([x_values[i]]))
+    # Loop through the chars
+    while True:
+        data = stream.read(1)
+        if 0 == len(data):
+            break
+        byte = data[0]
 
-  # Loop through the chars
-  while True:
-    data = stream.read(1)
-    if 0 == len(data):
-      break
-    byte = data[0]
+        keys = encode_byte(byte, n, k, x_values)
 
-    keys = encodeByte(byte,n,k,x_values)
-
-    for i in range(0,n):
-      outputs[i].write(keys[i])
+        for i in range(0, n):
+            outputs[i].write(keys[i])
 
 
-def decode(keys,output):
+def decode(keys, output):
+    interpolator = PGF256Interpolator()
+    zero = GF256elt(0)
 
-  interpolator = PGF256Interpolator()
-  zero = GF256elt(0)
+    # Read X values
+    X = []
+    for i in range(0, len(keys)):
+        data = keys[i].read(1)
+        if len(data) == 0:
+            raise Exception(f'Unexpected EOF while reading X of key {i}')
+        X.append(data[0])
 
-  # Read X values
-  X = []
-  for i in range(0, len(keys)):
-    data = keys[i].read(1)
-    if len(data) == 0:
-      raise Exception(f'Unexpected EOF while reading X of key {i}')
-    X.append(data[0])
+    end_of_key = False
+    while not end_of_key:
+        points = []
+        for i in range(0, len(keys)):
+            # Extract X/Y
+            data = keys[i].read(1)
+            if len(data) == 0:
+                end_of_key = True
+                break
+            Y = data[0]
 
-  end_of_key = False
-  while not end_of_key:
-    points = []
-    for i in range(0,len(keys)):
-      # Extract X/Y
-      data = keys[i].read(1)
-      if len(data) == 0:
-        end_of_key = True
-        break
-      Y = data[0]
+            # Push point
+            points.append((GF256elt(X[i]), GF256elt(Y)))
 
-      # Push point
-      points.append((GF256elt(X[i]),GF256elt(Y)))
+        if end_of_key:
+            if i != 0:
+                raise Exception(f'Unexpected EOF while reading key {i}')
+            break
 
-    if end_of_key:
-      if i != 0:
-        raise Exception(f'Unexpected EOF while reading key {i}')
-      break
-
-    # Decode next byte
-    byte = interpolator.interpolate(points).f(zero)
-    output.write(bytes([int(byte)]))
+        # Decode next byte
+        byte = interpolator.interpolate(points).f(zero)
+        output.write(bytes([int(byte)]))
 
 
 if __name__ == "__main__":
 
-  from io import BytesIO
-  input = BytesIO(b"Too many secrets, Marty!")
-  outputs = []
-  n = 5
-  k = 3
-  for i in range(n):
-    outputs.append(BytesIO())
+    from io import BytesIO
 
-  encode(input,outputs,k)
+    input = BytesIO(b"Too many secrets, Marty!")
+    outputs = []
+    n = 5
+    k = 3
+    for i in range(n):
+        outputs.append(BytesIO())
 
-  for i in range(n):
-    print (outputs[i].getvalue().hex())
+    encode(input, outputs, k)
 
-  inputs = []
-  for i in range(k):
-    inputs.append(outputs[i+1])
+    for i in range(n):
+        print(outputs[i].getvalue().hex())
 
-  for i in range(k):
-    inputs[i].seek(0)
+    inputs = []
+    for i in range(k):
+        inputs.append(outputs[i + 1])
 
-  output = BytesIO()
-  decode(inputs,output)
-  print (output.getvalue())
+    for i in range(k):
+        inputs[i].seek(0)
+
+    output = BytesIO()
+    decode(inputs, output)
+    print(output.getvalue())
