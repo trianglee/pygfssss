518806043278656c5cbc0ef442770d3162f7cc31
Nimrod Zimerman <zimerman@fastmail.fm>
2020-12-27 08:54:10 +0200

    Remove "coding" line from main file

--- pyssss/PySSSS.py
+++ pyssss/PySSSS.py
@@ -15,7 +15,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-# -*- coding: utf-8 -*-
+
 import secrets
 
 from pyssss.GF256elt import GF256elt
