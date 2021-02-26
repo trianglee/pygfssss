78b501303f9fdfcbf5bb0e7ee4b3371a2d84313a
Nimrod Zimerman <zimerman@fastmail.fm>
2021-02-26 23:15:42 +0200

    Preparation for PyPI release - setup.py and .gitignore

--- setup.py
+++ setup.py
@@ -3,9 +3,19 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
     name='pygfssss',
     version='0.0.0',
+    description="Python implementation of Shamir's Secret Sharing Scheme, using polynomials over GF(256)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/trianglee/pygfssss",
+    author="Nimrod Zimerman",
+    author_email="zimerman@fastmail.fm",
+    license="Apache License 2.0",
     packages=find_packages(),
     python_requires='>=3.7',
     install_requires=requirements,
